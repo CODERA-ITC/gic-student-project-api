9e7d6e85772a01290d37bfe00d66a6bb6e5afd92
Sovichet-Rathanak <rathanaksovichet@gmail.com>
2025-11-27 15:58:48 +0700

    entity/category: fix BaseEntity import

--- src/handlers/project/entities/category.entity.ts
+++ src/handlers/project/entities/category.entity.ts
@@ -1,5 +1,6 @@
-import { BaseEntity, Column, Entity, ManyToMany, OneToMany } from 'typeorm'
+import { Column, Entity, ManyToMany, OneToMany } from 'typeorm'
 import { Project } from './project.entity'
+import { BaseEntity } from 'src/database/base.entity'
 
 @Entity('categories')
 export class Category extends BaseEntity {
