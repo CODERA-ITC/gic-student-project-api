b33265372a315d64f559e1e7b77347efd5632a8c
chanmakara <sokchanmakara04@gmail.com>
2025-12-08 16:07:36 +0700

    add feature entity

--- src/handlers/project/entities/feature.entity.ts
+++ src/handlers/project/entities/feature.entity.ts
@@ -2,14 +2,21 @@ import { BaseEntity } from 'src/database/base.entity'
 import { Column, Entity, ManyToOne } from 'typeorm'
 import { Project } from './project.entity'
 
+export type FeatureStatus = "pending" | "ongoing" | "done";
 @Entity('features')
 export class Feature extends BaseEntity {
     @Column()
     name: string
 
+    @Column()
+    description: string
+
     // pending, ongoing, done
-    @Column({ default: 'pending' })
-    status: string
+    @Column({ default: 'pending', type:"enum", enum:["pending", "ongoing", "done"] })
+    status: FeatureStatus
+
+    @Column()
+    icon: string
 
     @ManyToOne(() => Project, project => project.features)
     project: Project
