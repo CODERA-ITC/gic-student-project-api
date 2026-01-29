d9f25c1746c334ec0fe26d41c915ec99706f7c46
Yem Daro <daroyem99@gmail.com>
2026-01-29 15:48:49 +0700

    remove endpoints for seeder service

--- src/handlers/seeder/seeder.controller.ts
+++ src/handlers/seeder/seeder.controller.ts
@@ -1,45 +1,44 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common'
-import { ApiOperation } from '@nestjs/swagger'
+import { Controller } from '@nestjs/common'
 import { SeederService } from './seeder.service'
 
 @Controller('seeder')
 export class SeederController {
-  constructor(private readonly seederService: SeederService) { }
+  constructor(private readonly seederService: SeederService) {}
 
-  @Post('/seed-all')
-  @ApiOperation({ summary: 'Seed all preliminary data' })
-  async seedAll() {
-    await this.seederService.seedDepartment()
-    await this.seederService.seedCategories()
-    await this.seederService.seedRole()
-    await this.seederService.seedTeachers()
-    await this.seederService.seedUser()
+  // @Post('/seed-all')
+  // @ApiOperation({ summary: 'Seed all preliminary data' })
+  // async seedAll() {
+  //   await this.seederService.seedDepartment()
+  //   await this.seederService.seedCategories()
+  //   await this.seederService.seedRole()
+  //   await this.seederService.seedTeachers()
+  //   await this.seederService.seedUser()
 
-    return 'Seeded: Department, Categories, Roles, Teachers, Users'
-  }
+  //   return 'Seeded: Department, Categories, Roles, Teachers, Users'
+  // }
 
-  @Post('/seed-categories')
-  async seedCategories() {
-    return await this.seederService.seedCategories()
-  }
+  // @Post('/seed-categories')
+  // async seedCategories() {
+  //   return await this.seederService.seedCategories()
+  // }
 
-  @Post('/seed-department')
-  seedDepartment() {
-    return this.seederService.seedDepartment()
-  }
+  // @Post('/seed-department')
+  // seedDepartment() {
+  //   return this.seederService.seedDepartment()
+  // }
 
-  @Post('/seed-role')
-  seedRole() {
-    return this.seederService.seedRole()
-  }
+  // @Post('/seed-role')
+  // seedRole() {
+  //   return this.seederService.seedRole()
+  // }
 
-  @Post('/seed-teachers')
-  seedTeachers() {
-    return this.seederService.seedTeachers()
-  }
+  // @Post('/seed-teachers')
+  // seedTeachers() {
+  //   return this.seederService.seedTeachers()
+  // }
 
-  @Post('/seed-users')
-  seedUsers() {
-    return this.seederService.seedUser()
-  }
+  // @Post('/seed-users')
+  // seedUsers() {
+  //   return this.seederService.seedUser()
+  // }
 }
